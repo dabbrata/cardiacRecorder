d3d5d1f05ba9c9bfe1897c00d565ce7eec18535c
dabbrata <83578877+dabbrata@users.noreply.github.com>
2022-07-17 22:45:27 +0600

    add some comments

--- app/src/androidTest/java/com/example/cardiacrecorder/MainActivityTest.java
+++ app/src/androidTest/java/com/example/cardiacrecorder/MainActivityTest.java
@@ -14,14 +14,35 @@ import org.junit.Rule;
 import org.junit.Test;
 
 public class MainActivityTest {
+    /**
+     * After main activity it will go to history activity
+     * The recyler view ui test of history activity causes some errors
+     * After providing correct code,the app was not run
+     * Thats why the profile activity still remain without ui testing in this project
+     * Because the recyler view are related with profile activity and
+     * Profile activity related with update activity
+     * Becasue some value are passes from one to another activity
+     * As a result the issues of ui test of recyler view causes another issues to another activity.
+     */
     @Rule
     public ActivityScenarioRule<MainActivity> activityRule =
             new ActivityScenarioRule<>(MainActivity.class);
 
+    /**
+     * here firstly check input field and teke input of user name
+     * then the other input field bpm,systolic pressure and dyastolic pressure will be filled
+     * the pressback inbuild function of expresso framework will be called
+     * after doing all these procedures, it will check to go to all history activity
+     * If the id of allhistory activity is similar with displayed activity ,then
+     * it will be considered correct of the ui design of the application
+     * @return void type testUserInfo method
+     *
+     */
+
     @Test
     public void testUserInfo()
     {
-        onView(withId(R.id.textInputEditText)).perform(ViewActions.typeText("Edmonton"));
+        onView(withId(R.id.textInputEditText)).perform(ViewActions.typeText("Erikson"));
         onView(withId(R.id.textInputEditText2)).perform(ViewActions.typeText("90"));
         onView(withId(R.id.textInputEditText3)).perform(ViewActions.typeText("140"));
         onView(withId(R.id.textInputEditText4)).perform(ViewActions.typeText("60"));
@@ -30,14 +51,28 @@ public class MainActivityTest {
         onView(withId(R.id.button)).perform(click());
         onView(withId(R.id.refreshButton)).perform(click());
         onView(withId(R.id.button3)).perform(click());
-
         onView(withId(R.id.recId)).check(matches(isDisplayed()));
     }
+
+    /**
+     * In mainactivity page, the cross button denote the exit of the application
+     * If the cross image button is clicked , then it will checked with the id of the button
+     * if there are some problems the system will be crushes
+     * @return void testExit method
+     */
     @Test
     public void testExit()
     {
         onView(withId(R.id.crossButton)).perform(click());
     }
+
+    /**
+     * In mainactivity page, the android backpress button denote the exit of the application.
+     * If the back button is clicked , then it will be called from Espresso framework and show alert
+     * for exit the app.
+     * if there are some problems in the alert dialog, the system will be crushes.
+     * @return void testExitBack method
+     */
     @Test
     public void testExitBack()
     {
