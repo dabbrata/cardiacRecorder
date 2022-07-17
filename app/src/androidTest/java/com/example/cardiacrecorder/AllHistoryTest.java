94d33677615a19606250c77f6fcb72cf57850e1e
Kowshik-Ahammed <kowshikahammed105@gmail.com>
2022-07-17 21:53:04 +0600

    all history ui test

--- app/src/androidTest/java/com/example/cardiacrecorder/AllHistoryTest.java
+++ app/src/androidTest/java/com/example/cardiacrecorder/AllHistoryTest.java
@@ -12,5 +12,21 @@ import org.junit.Rule;
 import org.junit.Test;
 
 public class AllHistoryTest {
+    @Rule
+    public ActivityScenarioRule<AllHistory> activityRule =
+            new ActivityScenarioRule<>(AllHistory.class);
+
+    /**
+     * ui test for all history page..,
+     * the back button is checked.
+     * then back to main activity
+     * then click history button to move to all history activity again
+     */
+    @Test
+    public void testItem(){
+        onView(withId(R.id.recId)).check(matches(isDisplayed()));
+        onView(withId(R.id.imageBackButton)).perform(click());
+        onView(withId(R.id.button3)).perform(click());
+    }
 
 }
